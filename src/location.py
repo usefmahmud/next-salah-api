0c0d41bef6ee693234526c1a0c7fe61d843280d5
Yousef Mahmoud <usef.mahmoud@outlook.com>
2024-12-21 16:26:39 +0200

    feat: Added Timezone finder method to Location

--- src/location.py
+++ src/location.py
@@ -1,4 +1,5 @@
 from geopy.geocoders import Nominatim
+from timezonefinder import TimezoneFinder
 
 
 class Location:
@@ -14,3 +15,11 @@ class Location:
         loc = geolocator.geocode(self.address)
 
         return (loc.latitude, loc.longitude)
+
+    def get_timezone(self) -> str:
+        '''
+        Get the timezone of a location using the latitude and longitude.
+        '''
+
+        tf = TimezoneFinder()
+        return tf.timezone_at(lat=self.get_lat_long()[0], lng=self.get_lat_long()[1])
