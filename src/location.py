03c4a144ec6373f0c196268ec0a5680a036cafa7
Yousef Mahmoud <usef.mahmoud@outlook.com>
2024-12-21 05:07:33 +0200

    Initialize Next Salah API with FastAPI and add location and salah functionality

---
+++ src/location.py
@@ -0,0 +1,15 @@
+from geopy.geocoders import Nominatim
+
+class Location:
+    def __init__(self, address: str):
+        self.address = address
+
+    def get_lat_long(self) -> tuple:
+        '''
+        Get the latitude and longitude of a location using the address.
+        '''
+
+        geolocator = Nominatim(user_agent='next-salah-api')
+        loc = geolocator.geocode(self.address)
+
+        return (loc.latitude, loc.longitude)
---
+++ src/salah.py
@@ -0,0 +1,13 @@
+import salat
+
+class Salah:
+
+    def __init__(self):
+        pass
+
+    def get_prev_next_salah(self):
+        '''
+        Get the previous and next salah times.
+        '''
+        
+        pass
